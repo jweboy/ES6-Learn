1c8e73a8196d9b949136d7a6ac14e0fdec598bda
jweboy <1259524459@qq.com>
2017-02-18 23:27:03 +0800

    add some new content

--- scripts/variableDeclaration.js
+++ scripts/variableDeclaration.js
@@ -52,4 +52,49 @@ console.log('end:'+i);
 for (let j = 0; j < 10; j++) {
   console.info('ing:'+j);
 }
-console.info('end:'+j);
+// console.info('end:'+j); //error
+
+// for (const h = 0; h< 5; h++) {//error
+//   console.log(h);
+// }
+
+var object = {
+  a:true,
+  b:false,
+  c:2
+};
+/**
+ * const变量可以在for-in和for-of中使用，同let
+ * 循环为每次迭代创建一个新的变量绑定，而不是试图修改已有的变量值
+ */
+for (const key in object) {
+  console.log(key);
+}
+for (const key of [1,2]) {
+  console.log(key);
+}
+
+/**
+ * var 声明全局变量，成为全局对象window的属性
+ * const与let声明，只会屏蔽全局变量，不会污染全局作用域
+ */
+console.group('全局块级绑定');
+console.log(window.alert);
+/**
+var alert = 'Hello World';
+const alert = 'Hello World';
+**/
+let alert = 'Hello World';
+console.log(alert);
+console.log(window.alert);
+console.groupEnd();
+
+/**
+ * 小结
+ *
+ *
+ * 最佳实践：
+ * 1.默认情况下用const
+ * 2.知道变量值需要更改时用let
+ * 3.全局用var
+ */
